bc686f8b9aad25a7ca1258febd5908dad75fa25a
decker <dkzmfhem@daum.net>
2024-09-25 20:36:07 +0900

    Node urlMap 리팩토링

--- NodeRounter.js
+++ NodeRounter.js
@@ -5,19 +5,21 @@ http.createServer((req, res) => {
     const path = url.parse(req.url, true).pathname;
     res.setHeader("Content-Type", "text/html");
 
-    if (path === "/user") {
-        user(req, res);
-    } else if (path === "/feed") {
-        feed(req, res);
-    } else {
+    if (path in urlMap){
+        urlMap[path](req, res);
+    }else {
         notFound(req, res)
     }
 }).listen(3000, ()=> console.log("Server Start"));
 
+const urlMap = {
+    "/" : (req, res) => res.end("Home"),
+    "/user" : user,
+    "/feed" : feed,
+}
 function user(req, res) {
     const userInfo = url.parse(req.url, true).query;
     res.end(`[user] name : ${userInfo.name}, age : ${userInfo.age}`);
-    
 }
 function feed(req, res) {
     res.end(`
